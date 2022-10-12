61054abb79af3892bb210574d883fe9c19962b35
HARITS <harits.muhammad.only@gmail.com>
2022-10-13 02:58:41 +0700

    Build Scrapindeed class

--- Scrapindeed/__init__.py
+++ Scrapindeed/__init__.py
@@ -1,25 +1,26 @@
 """
 All Function and Methode used in Scrapindeed
 """
-
+import requests
 # Import Eksternal Package
 import requests as re
 from bs4 import BeautifulSoup
 
 # Define parent class Scrapindeed
-class Scrapindeed:
+class Scrapindeed():
 
     # Define variable parent class Scrapindeed
-    def __init__(self, scheme, host, filename, header, result):
-        self.scheme = scheme
-        self.host = host
-        self.filename = filename
-        self.header = header
-        self.result = result
+    # def __init__(self, url, headers):
+    #     self.url = url
+    #     self.headers = {'User-Agent': headers}
 
     # Define function parent class Scrapindeed
 
+    # get url
+    def get_url(self, url, headers):
+        with re.Session() as session:
+            response = session.get(url, headers=headers)
+        return response
 
-# Define child class data engineer
 
 
