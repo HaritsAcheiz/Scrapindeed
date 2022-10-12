6d17cc789ea781957eac0113ee589d6db2548f14
Haritz <harits.muhammad.only@gmail.com>
2022-10-12 16:00:00 +0700

    init commit

---
+++ Scrapindeed/__init__.py
@@ -0,0 +1,25 @@
+"""
+All Function and Methode used in Scrapindeed
+"""
+
+# Import Eksternal Package
+import requests as re
+from bs4 import BeautifulSoup
+
+# Define parent class Scrapindeed
+class Scrapindeed:
+
+    # Define variable parent class Scrapindeed
+    def __init__(self, scheme, host, filename, header, result):
+        self.scheme = scheme
+        self.host = host
+        self.filename = filename
+        self.header = header
+        self.result = result
+
+    # Define function parent class Scrapindeed
+
+
+# Define child class data engineer
+
+
---
+++ main.py
@@ -0,0 +1,3 @@
+"""
+This Code created for place to learn webscraping on indeed.com only by requests and beautifulsoup4
+"""
