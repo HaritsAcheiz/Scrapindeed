4fbca00dd9b59adf4c10fc220a6c59488cca56e6
HARITS <harits.muhammad.only@gmail.com>
2022-10-15 06:07:05 +0700

    web driver get only 1 url

--- Scrapindeed/__init__.py
+++ Scrapindeed/__init__.py
@@ -1,11 +1,17 @@
 """
 All Function and Methode used in Scrapindeed
 """
-import requests
+
 # Import Eksternal Package
-import requests as re
+import requests
 from bs4 import BeautifulSoup
 import cloudscraper
+from selenium.webdriver.firefox.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
 
 
 # Define parent class Scrapindeed
@@ -18,15 +24,32 @@ class Scrapindeed():
 
     # Define function parent class Scrapindeed
 
-    # get url
-    def get_url(self, url, headers):
-        scraper = cloudscraper.create_scraper(disableCloudflareV1 = True)
-        response = scraper.get(url)
-        response = BeautifulSoup(response.text, 'html.parser')
-        # with re.Session() as session:
-        #     response = session.get(url, headers=headers)
-        #     response = BeautifulSoup(response.text, 'html.parser')
-        return response
+    # def get_url(self, url, headers):
+    #     with requests.Session() as session:
+    #         response = session.get(url, headers=headers)
+    #     session.close()
+    #     return response
 
+    # def get_url(self, url, headers, interpreter):
+    #     scraper = cloudscraper.create_scraper(disableCloudflareV1 = True, interpreter=interpreter)
+    #     response = scraper.get(url, headers=headers)
+    #     return response
 
+    def get_url(self, url, webdriver_path, headers):
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-translate")
+        options.add_argument(headers['User - Agent'])
+        driver = webdriver.Firefox(executable_path=webdriver_path, options=options)
+        driver.get(url)
+        response = WebDriverWait(driver, 30).until(ec.presence_of_element_located((By.ID, 'jobsearch-Main')))
+        parent = response.find_element(By.CSS_SELECTOR, '.jobsearch-ResultsList')
+        children = parent.find_elements(By.TAG_NAME, 'li')
+        result = []
+        for i in children:
+            data = i.find_element(By.XPATH, "//a[@class='jcs-JobTitle css-jspxzf eu4oa1w0']").get_attribute('href')
+            result.append(data)
 
+        return result
--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from Scrapindeed import Scrapindeed
 if __name__ == '__main__':
     url = 'https://id.indeed.com/jobs?q=data+engineer&l=Tangerang&from=searchOnHP&vjk=2d21e19686fcde5e'
     header = {
-        'User - Agent': 'Mozilla / 5.0(Windows NT 10.0; Win64; x64; rv: 105.0) Gecko / 20100101 Firefox / 105.0',
-        'Referer': 'https: // id.indeed.com / jobs?q = data + engineer & l = Tangerang & from=searchOnHP & vjk = 2 d21e19686fcde5e',
+        'User - Agent': 'Mozilla / 5.0(Windows NT 10.0; Win64; x64; rv: 105.0) Gecko / 20100101 Firefox / 105.0'
     }
+    webdriverpath = 'C:/geckodriver-v0.31.0-win64/geckodriver.exe'
     scraper = Scrapindeed()
-    response = scraper.get_url(url, headers=header)
-    print(response)
+    response = scraper.get_url(url, webdriver_path=webdriverpath, headers=header)
+    for i in response:
+        print(i)
